4be78cd070d83568e12d255875e59b9a4ab09575
Matthew O'Mara <tuh39725@temple.edu>
2022-03-21 21:31:28 -0400

    initial commit ignore and main

---
+++ perfect_playlist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Emma Dunsinger, Matthew O'Mara, Evan Noyes, Tommy Ngo"
+__version__ = "0.1.0"
+__license__ = "N/A"
+
+def main():
+    
+    print("hello temple")
+
+
+if __name__ == "__main__":
+    main()
