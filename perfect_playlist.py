91c2d9b40ffe1b585043dbf50e3399db2ba7eb0b
tui92851 <tui92851@temple.edu>
2022-03-30 11:43:27 -0400

    Revert "get_song added"
    This reverts commit 9e30dded071bfaa8b3d7ea213bd4cd12b57296e7.

--- perfect_playlist.py
+++ perfect_playlist.py
@@ -12,8 +12,6 @@ import imageio
 import spotipy
 import os
 import json
-import typer
-
 from spotipy.oauth2 import SpotifyClientCredentials
 
 ##Spotify API credentials here##
@@ -23,12 +21,15 @@ SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
 
 def call_api():
 
+    ## set environment varialbes
+    os.environ['SPOTIPY_CLIENT_ID'] = str(SPOTIPY_CLIENT_ID)
+    os.environ['SPOTIPY_CLIENT_SECRET'] = str(SPOTIPY_CLIENT_SECRET)
+    os.environ['SPOTIPY_REDIRECT_URI'] = str(SPOTIPY_REDIRECT_URI)
 
-
-    sp = get_spotify_client()
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
     results = sp.playlist('spotify:user:spotifycharts:playlist:37i9dQZEVXbMDoHDwVN2tF')
-
     # print(json.dumps(results, indent=4))
 
     # print(results.keys())
@@ -46,48 +47,11 @@ def call_api():
             item['track']['artists'][0]['name'] + ' - ' +
             item['track']['album']['name']
             )
-    print("\n### END OF TOP 50 PLAYLIST ### \n")
 
 
-
-def get_spotify_client() -> spotipy.Spotify:
-    """
-    It creates a Spotify client using the spotipy library
-    :return: spotipy.Spotify object
-    """
-    setup_env_vars()
-    auth_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    return sp
-
-def setup_env_vars():
-    ## set environment varialbes
-    os.environ['SPOTIPY_CLIENT_ID'] = str(SPOTIPY_CLIENT_ID)
-    os.environ['SPOTIPY_CLIENT_SECRET'] = str(SPOTIPY_CLIENT_SECRET)
-    os.environ['SPOTIPY_REDIRECT_URI'] = str(SPOTIPY_REDIRECT_URI)
-
-
-def get_song():
-    songName = input("type in a song title to search:  ")
-    sp = get_spotify_client()
-
-    results = sp.search(q = songName, type='track', limit=5)
-    possible_songs = results.get('tracks').get('items')
-
-    song_list_str = ''
-    for i, song in enumerate(possible_songs):
-        song_list_str += f'{i+1}. {song["name"]} by {song["artists"][0]["name"]}\n'
-    song_selection = input(f'\n{song_list_str}\n\nEnter the number of the song you want to add: ')
-    //TO BE COMPLETED
 
 if __name__ == "__main__":
     img = imageio.imread("images.png")
     input('press ENTER to start')
     call_api()
-
-    ## START of get song
-    get_song()
-
-
-
     input('press enter to exit')
