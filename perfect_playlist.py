e52afac43da442aab7605262c3d8e689beaed0bc
Evan Noyes <evan.noyes@temple.edu>
2022-03-31 20:31:59 -0400

    opens image from filename input by user

--- perfect_playlist.py
+++ perfect_playlist.py
@@ -12,8 +12,6 @@ import imageio
 import spotipy
 import os
 import json
-import typer
-
 from spotipy.oauth2 import SpotifyClientCredentials
 
 ##Spotify API credentials here##
@@ -21,70 +19,41 @@ SPOTIPY_CLIENT_ID='9f4d5ba1da544502a6bdb038d16cf067'
 SPOTIPY_CLIENT_SECRET='f54225b24200404baca25763718bd7c8'
 SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
 
-def get_top50():
-    sp = get_spotify_client()
-    results = sp.playlist('spotify:user:spotifycharts:playlist:37i9dQZEVXbMDoHDwVN2tF')
+def call_api():
 
+    ## set environment varialbes
+    os.environ['SPOTIPY_CLIENT_ID'] = str(SPOTIPY_CLIENT_ID)
+    os.environ['SPOTIPY_CLIENT_SECRET'] = str(SPOTIPY_CLIENT_SECRET)
+    os.environ['SPOTIPY_REDIRECT_URI'] = str(SPOTIPY_REDIRECT_URI)
+
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    results = sp.playlist('spotify:user:spotifycharts:playlist:37i9dQZEVXbMDoHDwVN2tF')
+    # print(json.dumps(results, indent=4))
+
+    # print(results.keys())
+    # items = results.get('items')
+    # print(json.dumps(items, indent=4))
+
+    # print(items["name"])
+    # for key, value in results.items():
+    #     print(key, ' : ', value)
     print("\nSong - Artist - Album\n")
+
     for item in results['tracks']['items']:
             print(
             item['track']['name'] + ' - ' +
             item['track']['artists'][0]['name'] + ' - ' +
             item['track']['album']['name']
             )
-    print("\n### END OF TOP 50 PLAYLIST ### \n")
 
-
-
-def get_spotify_client() -> spotipy.Spotify:
-    """
-    It creates a Spotify client using the spotipy library
-    :return: spotipy.Spotify object
-    """
-    setup_env_vars()
-    auth_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    return sp
-
-def setup_env_vars():
-    ## set environment varialbes
-    os.environ['SPOTIPY_CLIENT_ID'] = str(SPOTIPY_CLIENT_ID)
-    os.environ['SPOTIPY_CLIENT_SECRET'] = str(SPOTIPY_CLIENT_SECRET)
-    os.environ['SPOTIPY_REDIRECT_URI'] = str(SPOTIPY_REDIRECT_URI)
-
-
-def get_song():
-    songName = input("Type in a song title to search:  ")
-    sp = get_spotify_client()
-
-    results = sp.search(q = songName, type='track', limit=5)
-    possible_songs = results.get('tracks').get('items')
-
-    song_list_str = ''
-    for i, song in enumerate(possible_songs):
-        song_list_str += f'{i+1}. {song["name"]} by {song["artists"][0]["name"]}\n'
-    song_selection = input(f'\n{song_list_str}\n\nEnter the number of the song you want to select: ')
-    assert song_selection.isdigit(), 'Please enter a number'
-    assert int(song_selection) <= len(possible_songs), 'Please enter a number between 1 and {}'.format(len(possible_songs))
-    selected_song = possible_songs[int(song_selection)-1]
-    print(f'[option={int(song_selection)}, query="{songName}"]')
-    print(f'>>>\n>>> {selected_song["name"]} by {selected_song["artists"][0]["name"]}\n>>>\n')
-
-
-
-    #TO BE COMPLETED
+def get_image():
+    imgpath = input('Input an image filepath\n')
+    return imageio.imread(imgpath)
 
 if __name__ == "__main__":
-    ##img = imageio.imread("images.png")
-    input('press ENTER to start\n')
-
-    ## START of get_top50 (formerly call_api)
-    '''
-    get_top50()
-    '''
-    ## START of get song
-    get_song()
-
-
-
+    input('press ENTER to start')
+    img = get_image()
+    call_api()
     input('press enter to exit')
